8d8fc15899e6488a629c19c269b3bed429fe5836
tamilarasan1309 <tamilaharasan@gmail.com>
2025-03-05 18:16:13 +0530

    String Variable

---
+++ String.dart
@@ -0,0 +1,8 @@
+void main(){
+  //This Program is for String Literals and Interpolation
+  String Name ="Vicky";
+  int l =20;
+  int b =10;
+  print("My Name is $Name");
+  print("Area of the Rectangle is ${l*b}");
+}
